0eb640adfdc4c9a31e330f1d149e96ca93dce651
TomozQ <inf.4clover@gmail.com>
2021-12-05 21:59:28 +0900

    class-validator

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import 'reflect-metadata'
 import { plainToClass } from 'class-transformer'
+import { validate } from 'class-validator'
+
 import { Product } from './product.model'
 
 const products = [
     {title: '商品１', price: 100},
     {title: '商品２', price: 200},
 ]
+
+const newProd = new Product('', -100)
+validate(newProd).then(errors => {
+    if(errors.length > 0){
+        console.log('validation error!')
+        console.log(errors)
+    }else{
+        console.log(newProd.getInformation())
+    }
+})
 
 // const p1 = new Product('商品１', 100)
 
